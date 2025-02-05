53de3bb296502cf36a59a0392a4815f110219c5c
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-02-05 19:27:29 +0530

    fetch the data for both products and user

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,6 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "./SectionTitle";
+import axios from "axios";
 
 const ProductsList = () => {
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const { data } = await axios.get("https://api.restful-api.dev/objects");
+      return data;
+    },
+  });
+
+  console.log(products);
+
+  if (isLoading) {
+    return <div className="text-center font-bold h-screen">Loading ...</div>;
+  }
+
   return (
     <div className="max-w-7xl mx-auto p-4">
       <SectionTitle heading="All Products"></SectionTitle>
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,6 +1,23 @@
+import axios from "axios";
 import SectionTitle from "./SectionTitle";
+import { useQuery } from "@tanstack/react-query";
 
 const UsersList = () => {
+  const { data: users = [], isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return data;
+    },
+  });
+
+  console.log(users);
+
+  if (isLoading) {
+    return <div className="text-center font-bold h-screen">Loading ...</div>;
+  }
   return (
     <div className="max-w-7xl mx-auto p-5">
       <SectionTitle heading="user list"></SectionTitle>
